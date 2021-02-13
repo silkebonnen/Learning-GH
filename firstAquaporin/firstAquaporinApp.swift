6d2fe399d7113b4a9d5215bb88e5731cfa1aeb57
Silke Bonnen <silke.bonnen@gmail.com>
2021-02-13 13:09:58 +0100

    Initial Commit

---
+++ firstAquaporin/firstAquaporinApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstAquaporinApp.swift
+//  firstAquaporin
+//
+//  Created by Silke Sofie Holme Bonnen on 13/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct firstAquaporinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
